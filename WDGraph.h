dc08fb17712127b8ae808a8f121b9c2dae214bba
Themeralderp <48189298+TheEmeraldDerpLeader@users.noreply.github.com>
2024-04-18 13:49:15 -0400

    Updated graph and added parser for airport codes

--- WDGraph.h
+++ WDGraph.h
@@ -13,7 +13,13 @@ public:
 
     void add_vertex(const WDVertex& ver);
     void add_edge(const WDVertex& ver1, const WDVertex& ver2, int distance, int cost); //connect ver1 to ver2
+    
     int get_vertex_index(const WDVertex& ver) const;
+    const WDVertex& GetVertexAtIndex(int index) const;
+
+    int SearchForCodeIndex(char* code); //code should point to three characters
+    const WDVertex& SearchForCode(char* code); //code should point to three characters
+
 
     void print() const;
 
